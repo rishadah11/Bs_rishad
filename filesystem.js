a9c5e94530b219475947b0b76908613960aff385
Abdul Awal <16172103375@cse.bubt.edu.bd>
2021-07-10 16:37:45 +0600

    first_commit

---
+++ filesystem.js
@@ -0,0 +1,9 @@
+var file=require('fs');
+var http=require('http');
+var url=require('url');
+
+var server=http.createServer(function (req,res){
+    fs.readFile( function (){
+       if()
+    })
+})
